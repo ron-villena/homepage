a25c937bcb37dd3cec9e314155f2b2ea2ce7f5cf
Ron Villena <ron_villena@hotmail.com>
2020-03-27 16:06:29 -0700

    Added some templating, clean up bugs and go rid of old code

--- build.py
+++ build.py
@@ -1,7 +1,3 @@
-# old template
-# template_top = open('./templates/template_top.html').read()
-# template_bottom = open('./templates/template_bottom.html').read()
-
 # Base template: Consists of header and footer for pages (static)
 template_base = open('./templates/base.html').read()
 
@@ -15,12 +11,12 @@ pages = [
     {
         'filename': './content/resume.html',
         'output': './docs/resume.html',
-        'title': 'Resume'
+        'title': 'My Resume'
     },
     {
         'filename': './content/blog.html',
         'output': './docs/blog.html',
-        'title': 'blog'
+        'title': 'My Blog'
     }
 ]
 
@@ -31,37 +27,15 @@ def main():
 
 # Method: creates actual page; param: dict for specific page
 def create_page(page_dic):
-    open(page_dic['output'], 'w+').write(assemble_page(page_dic['filename']))
+    open(page_dic['output'], 'w+').write(assemble_page(page_dic['filename'], page_dic['title']))
 
 # Method: combines base and content template; param: content html code
-def assemble_page(middle_filename):
+def assemble_page(middle_filename, title):
     template_middle = open(middle_filename).read()
     # print(template_middle)
     # print(template_base)
     complete_html = template_base.replace("{{content}}", template_middle)
+    complete_html = complete_html.replace("{{title}}", title)
     return complete_html
 
-
 main()
-
-# HW2 Code
-# template_top = open('./templates/template_top.html').read()
-# template_bottom = open('./templates/template_bottom.html').read()
-#
-# template_index = open('./content/index.html').read()
-#
-# # Create index.html
-# index_html = template_top + template_index + template_bottom
-# open('./docs/index.html', 'w+').write(index_html)
-#
-# template_resume = open('./content/resume.html').read()
-#
-# # Create resume.html
-# resume_html = template_top + template_resume + template_bottom
-# open('./docs/resume.html', 'w+').write(resume_html)
-#
-# template_blog = open('./content/blog.html').read()
-#
-# # Create blog.html
-# blog_html = template_top + template_blog + template_bottom
-# open('./docs/blog.html', 'w+').write(blog_html)
